7a760e63233fae765e77fe74b59bad68cd0b28c4
Mike Barberry <mikebarberry@protonmail.com>
2020-08-31 17:54:50 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,6 +5,13 @@ def fizzbuzz(int)
   if int % 3 == 0
     puts "Fizz"
   end
+elseif int % 5 == 0
+  puts "Buzz"
+end 
+elseif int % 3 == 0 && int % 5 == 0 
+  puts "FizzBuzz"
+else 
+  puts 
 end
 
 fizzbuzz(3)
